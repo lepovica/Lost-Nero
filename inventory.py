6defd46a94eb2f4beb93423e48f497304770971c
lepovica <lepo333@abv.bg>
2014-06-19 15:15:22 +0300

    add draw functions

--- inventory.py
+++ inventory.py
@@ -16,6 +16,8 @@ AXES_IMG = list()
 STAFF_IMG = list()
 BOW_IMG = list()
 
+SWORDS = dict()
+
 for i in range(47):
     SWORD_IMG.append(pygame.image.load('sw'+str(i+1)+'.png'))
     AXES_IMG.append(pygame.image.load('sw'+str(i+1)+'.png'))
@@ -23,52 +25,43 @@ for i in range(47):
     BOW_IMG.append(pygame.image.load('sw'+str(i+1)+'.png'))
 
 
-# class Item(pygame.sprite.Sprite):
-#     (DROPPED, GETTED) = range(2)
+class Item(pygame.sprite.Sprite):
+    (DROPPED, GETTED) = range(2)
 
-#     def __init__(self, screen, pos, img_file, item_type,
-#                  price, attack_power, deffence_power, armor, name, level):
+    def __init__(self, screen, pos, img_file, item_type,
+                 price, attack_power, deffence_power, armor, name, level):
 
-#         pygame.sprite.Sprite.__init__(self)
-#         self.pos = vec2d(pos)
-#         self.screen = screen
-#         self.base_image = img_file
-#         self.image = self.base_image
-#         self.state = self.DROPPED
-#         self.type = item_type
-#         self.attack_power = attack_power
-#         self.deffence_power = deffence_power
-#         self.armor = armor
-#         self.price = price
-#         self.name = name
-#         self.level_required = level
+        pygame.sprite.Sprite.__init__(self)
+        self.pos = vec2d(pos)
+        self.screen = screen
+        self.base_image = img_file
+        self.image = self.base_image
+        self.state = self.DROPPED
+        self.type = item_type
+        self.attack_power = attack_power
+        self.deffence_power = deffence_power
+        self.armor = armor
+        self.price = price
+        self.name = name
+        self.level_required = level
 
-#     def drop(self, pos):
-#         self.state = self.DROPPED
+    def drop(self, pos):
+        self.state = self.DROPPED
 
-#     def get(self):
-#         self.state = self.GETTED
+    def get(self):
+        self.state = self.GETTED
 
-#     def draw(self):
-#         item_rect = self.image.get_rect().move(
-#             self.pos.x - self.image.get_size()[0] / 2,
-#             self.pos.y - self.image.get_size()[1] / 2)
-#         self.screen.blit(self.image, item_rect)
+    def draw(self):
+        item_rect = self.image.get_rect().move(
+            self.pos.x - self.image.get_size()[0] / 2,
+            self.pos.y - self.image.get_size()[1] / 2)
+        self.screen.blit(self.image, item_rect)
 
 
 
-# SWORDS = dict()
 
-# for img_file in SWORD_IMG:
-#     i += i + 1
 
-#     SWORDS[str(i) + "_sword"] = type(str(i) + "_sword", (Item,), {
-#                            'pos' : vec2d((0, 0)), 'screen' : display,
-#                            'base_image' : img_file, 'image' : img_file, 
-#                            'state' : 0, 'item_type' : ITEM_TYPES[0], 
-#                            'level_required' : level, 'name' : str(i) + "_sword",
-#                            'attack_power' : level*20, 'deffence_power' : level*5,
-#                            'price' : level*100, 'armor' : level*100 })
+
 
 
 class Cell(pygame.sprite.Sprite):
@@ -78,17 +71,15 @@ class Cell(pygame.sprite.Sprite):
         self.item = item
         self.screen = screen
         self.background = background
-        self.side = 60
+        self.side = 50
 
     def is_empty(self):
         return self.item == None
 
     def draw():
-        cell_rect = None
         if self.background == None:
-            cell_rect = self.texture.get_rect()
-        else:
-            cell_rect = self.background.get_rect()
+            self.background = Color('Black')
+        cell_rect = self.background.get_rect()
 
         cell_rect.move(
                 self.pos.x - self.image.get_size()[0] / 2,
@@ -109,10 +100,21 @@ class Inventory:
         self.screen = screen
         self.bag_pages = 1
         self.bag = [[]]
+        self.start_pos_bag = vec2d(0, 0)
+        # initial of bag
         self.inverntory = list()
+        self.start_pos_inventory = vec2d(0, 0)
         for i in range(11):
-            self.inverntory.append(None)
+            current_pos = self.start_pos_inventory
+            self.inverntory.append(Cell(self.screen, current_pos))
+            current_pos.x += 60
         self.bar = []
+        self.start_pos_bar = vec2d(5, 545)
+        for i in range(9):
+            current_pos = self.start_pos_bar
+            self.bar.append(Cell(self.screen, current_pos)) 
+            current_pos.x += 60
+
 
     def add_item_bag(self, item):
         if self.bag_pages <= 3:
@@ -158,8 +160,39 @@ class Inventory:
             step_h_counter = 0
 
 
+    def load_items(self):
+        for img_file in SWORD_IMG:
+            i += i + 1
+
+            SWORDS[str(i) + "_sword"] = type(str(i) + "_sword", (Item,), {
+                                   'pos' : vec2d(0, 0), 'screen' : self.screen,
+                                   'base_image' : img_file, 'image' : img_file, 
+                                   'state' : 0, 'item_type' : ITEM_TYPES[0], 
+                                   'level_required' : level, 
+                                   'name' : str(i) + "_sword",
+                                   'attack_power' : level*20, 
+                                   'deffence_power' : level*5,
+                                   'price' : level*100, 'armor' : level*100 })
+
+            # load more items..
+
+
 
     def draw_inventory(self):
+        for cell in self.inverntory:
+            cell.draw()
+            cell.item.draw()
+
+            
+
+    def draw_bar(self):
+        for cell in self.bar:
+            cell.draw()
+            cell.item.draw()
+
+
+
+    def draw_bag(self):
         pass
 
 
